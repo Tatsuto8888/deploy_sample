4295a8c9b961dc165e785a852a52b99cd4b4496d
Tatsuto8888 <longdouzhongcun500@gmail.com>
2025-04-18 22:41:51 +0900

    fix:日本語エラー表記の修正

--- app/models/saving.rb
+++ app/models/saving.rb
@@ -2,5 +2,5 @@ class Saving < ApplicationRecord
   belongs_to :goal
 
   validates :amount, presence: { message: "を入力してください" },
-                     numericality: { only_integer: true, message: "金額は半角数字で入力してください" }
+                     numericality: { only_integer: true, message: "は半角数字で入力してください" }
 end
